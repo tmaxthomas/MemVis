9fd56d6bfbfdef8d43ab170cc78d977665c9ba95
tmaxthomas <t_max_thomas@hotmail.com>
2018-04-11 19:03:14 -0400

    Ran memTest

--- dr_plugin/build.sh
+++ dr_plugin/build.sh
@@ -5,3 +5,8 @@ cd build
 cmake -D CMAKE_C_COMPILER=/usr/bin/clang ..
 make
 cp *.so ..
+cd ../../../dynamorio-master
+cmake .
+make -j4
+mv drcachetrace.drrun64 /usr/local/DynamoRIO/tools
+mv clients/bin64/drcachetrace /usr/local/DynamoRIO/tools/bin64
