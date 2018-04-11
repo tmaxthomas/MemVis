398ebfe74c53d3a5af71a679d3f95ad08ce5cf01
Glenn Smith <couleeapps@gmail.com>
2018-04-11 18:28:46 -0400

    Updated memtest to hopefully be less bad

--- memTest/main.cpp
+++ memTest/main.cpp
@@ -10,13 +10,120 @@ struct Bits {
 	int height;
 	const char *layout;
 };
-static std::vector<Bits> chars{
+static Bits chars[256] = {
+	{}, // 0
+	{}, // 1
+	{}, // 2
+	{}, // 3
+	{}, // 4
+	{}, // 5
+	{}, // 6
+	{}, // 7
+	{}, // 8
+	{}, // 9
+	{}, // 10
+	{}, // 11
+	{}, // 12
+	{}, // 13
+	{}, // 14
+	{}, // 15
+	{}, // 16
+	{}, // 17
+	{}, // 18
+	{}, // 19
+	{}, // 20
+	{}, // 21
+	{}, // 22
+	{}, // 23
+	{}, // 24
+	{}, // 25
+	{}, // 26
+	{}, // 27
+	{}, // 28
+	{}, // 29
+	{}, // 30
+	{}, // 31
+	{' ', 2, 1,
+		"  "},
+	{'!', 1, 5,
+		"."
+		"."
+		"."
+		" "
+		"."},
+	{}, // 34
+	{}, // 35
+	{}, // 36
+	{}, // 37
+	{}, // 38
+	{}, // 39
+	{}, // 40
+	{}, // 41
+	{}, // 42
+	{}, // 43
+	{',', 2, 2,
+		" ."
+		". "},
+	{}, // 45
+	{}, // 46
+	{}, // 47
+	{}, // 48
+	{}, // 49
+	{}, // 50
+	{}, // 51
+	{}, // 52
+	{}, // 53
+	{}, // 54
+	{}, // 55
+	{}, // 56
+	{}, // 57
+	{}, // 58
+	{}, // 59
+	{}, // 60
+	{}, // 61
+	{}, // 62
+	{}, // 63
+	{}, // 64
+	{}, // 65
+	{}, // 66
+	{}, // 67
+	{}, // 68
+	{}, // 69
+	{}, // 70
+	{}, // 71
+	{}, // 72
+	{}, // 73
+	{}, // 74
+	{}, // 75
+	{}, // 76
+	{}, // 77
+	{}, // 78
+	{}, // 79
+	{}, // 80
+	{}, // 81
+	{}, // 82
+	{}, // 83
+	{}, // 84
+	{}, // 85
+	{}, // 86
+	{}, // 87
+	{}, // 88
+	{}, // 89
+	{}, // 90
+	{}, // 91
+	{}, // 92
+	{}, // 93
+	{}, // 94
+	{'_', 4, 1,
+		"...."},
+	{}, // 96
 	{'a', 4, 5,
 		" .. "
 		"   ."
 		" ..."
 		".  ."
 		" ..."},
+	{},
 	{'c', 4, 4,
 		" ..."
 		".   "
@@ -46,18 +153,22 @@ static std::vector<Bits> chars{
 		" ..."
 		"   ."
 		" .. "},
+	{},
 	{'i', 2, 5,
 		". "
 		"  "
 		". "
 		". "
 		" ."},
+	{},
+	{},
 	{'l', 2, 5,
 		". "
 		". "
 		". "
 		". "
 		" ."},
+	{},
 	{'n', 4, 4,
 		"... "
 		".  ."
@@ -68,76 +179,184 @@ static std::vector<Bits> chars{
 		".  ."
 		".  ."
 		" .. "},
+	{},
+	{},
 	{'r', 4, 4,
 		". .."
 		"..  "
 		".   "
 		".   "},
+	{},
 	{'t', 4, 5,
 		" .  "
 		"...."
 		" .  "
 		" .  "
 		"  .."},
-	{' ', 2, 1,
-		"  "},
-	{',', 2, 2,
-		" ."
-		". "},
-	{'!', 1, 5,
-		"."
-		"."
-		"."
-		" "
-		"."},
+	{}, // 117
+	{}, // 118
+	{}, // 119
+	{}, // 120
+	{}, // 121
+	{}, // 122
 	{'{', 4, 5,
 		"  .."
 		" .  "
 		"..  "
 		" .  "
 		"  .."},
+	{}, // 124
 	{'}', 4, 5,
 		"..  "
 		"  . "
 		"  .."
 		"  . "
 		"..  "},
-	{'_', 4, 1,
-		"...."},
-};
+	{}, // 126
+	{}, // 127
 
-const Bits &chBits(char ch) {
-	return *std::find_if(chars.begin(), chars.end(), [ch](Bits &bits)->bool {
-		return bits.ch == ch;
-	});
-}
+};
 
 int main(int argc, const char **argv) {
 	static const char *str = "flag{i_need_a_doctor}";
+	static int length = 21;
 
 	//Find length of str
 	int width = 0;
 	int height = 0;
-	for (int i = 0; i < strlen(str); i ++) {
-		const Bits &bits = chBits(str[i]);
-		width += bits.width + 1;
-		height = std::max(height, bits.height);
+	for (int i = 0; i < length; i ++) {
+		Bits *bits = &(chars[(int)str[i]]);
+		width += bits->width + 1;
+		if (bits->height > height)
+			height = bits->height;
 	}
-	if (width & 0xF) width += 0x10 - (width % 0x10);
+	width = 2048;
 	//Build memory accesses for str
 	unsigned char *memories = new unsigned char[width * height]();
 
 	int lead = 0;
-	for (int i = 0; i < strlen(str); i ++) {
-		const Bits &bits = chBits(str[i]);
+	for (int i = 0; i < length; i ++) {
+		Bits *bits = &(chars[(int)str[i]]);
 
-		int sy = height - bits.height;
+		int sy = height - bits->height;
 		int sx = lead;
 
-		for (int y = 0; y < bits.height; y ++) {
-			for (int x = 0; x < bits.width; x ++) {
-				if (bits.layout[x + y * bits.width] != ' ') {
+		for (int y = 0; y < bits->height; y ++) {
+			for (int x = 0; x < bits->width; x ++) {
+				if (bits->layout[x + y * bits->width] != ' ') {
 					unsigned char *memptr = &memories[sx + x + ((sy + y) * width)];
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
+					(*memptr)++;
 					(*memptr)++;
 					(*memptr)++;
 					(*memptr)++;
@@ -198,7 +417,7 @@ int main(int argc, const char **argv) {
 			}
 		}
 
-		lead += bits.width + 1;
+		lead += bits->width + 1;
 	}
 
 	delete [] memories;
